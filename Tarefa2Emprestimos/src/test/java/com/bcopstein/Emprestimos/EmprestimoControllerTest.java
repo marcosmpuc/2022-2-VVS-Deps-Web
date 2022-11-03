76933d7536eec6c53c3849a63bc1115fe4517a3a
Isabella Becker <isabella.becker@edu.pucrs.br>
2022-11-03 15:33:43 -0300

    teste de integração juro simples

--- Tarefa2Emprestimos/src/test/java/com/bcopstein/Emprestimos/EmprestimoControllerTest.java
+++ Tarefa2Emprestimos/src/test/java/com/bcopstein/Emprestimos/EmprestimoControllerTest.java
@@ -1,22 +1,33 @@
 package com.bcopstein.Emprestimos;
 
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import static org.assertj.core.api.Assertions.assertThat;
-
-@WebMvcTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class EmprestimoControllerTest {
 
-    @InjectMocks
-    private EmprestimoController emprestimoController;
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
-    void emprestimoJurosSimples() {
-        EmprestimoDTO dtoComp = new EmprestimoDTO(true,false,1000,0.035,5,
-                1285.0, 257.0);
-        assertThat(emprestimoController.emprestimoJurosSimples(1000.0,5,0.035)).isEqualToComparingFieldByFieldRecursively(dtoComp);
+    void emprestimoJurosSimples() throws Exception {
+        RequestBuilder rb = MockMvcRequestBuilders.get("/emprestimo/jurosSimples?valor=1000&parcelas=5&taxa=0.035");
+        MvcResult result = this.mockMvc.perform(rb).andReturn();
 
+        EmprestimoDTO espDto = new EmprestimoDTO(true,false,1000,0.035,5,
+                1285.0, 257.0);
+
+        Gson gson = new Gson();
+        String espJasonS = gson.toJson(espDto);
+        Assertions.assertEquals(espJasonS, result.getResponse().getContentAsString());
     }
 
     @Test
